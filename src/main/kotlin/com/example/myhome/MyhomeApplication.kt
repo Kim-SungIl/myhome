04d48450f57431b10e37345d078bb5cd4e1a73e0
kevin.kim <joongjum@naver.com>
2023-12-02 00:00:09 +0900

    최초 업데이트

---
+++ src/main/kotlin/com/example/myhome/MyhomeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.myhome
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyhomeApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyhomeApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/myhome/component/HttpClient.kt
@@ -0,0 +1,73 @@
+package com.example.myhome.component
+
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
+
+@Component
+class HttpClient (val restTemplate: RestTemplate){
+    fun callExternalGet(apiUrl:String, parameters:Map<String, Any>): String {
+        // 매개변수를 포함한 URI를 빌드
+        val uri = UriComponentsBuilder.fromUriString(apiUrl)
+            .apply {
+                parameters.forEach { (key, value) -> queryParam(key, value) }
+            }
+            .build()
+            .toUriString()
+
+        // API 호출
+        val response = restTemplate.getForObject(uri, String::class.java)
+
+        // response 처리
+        return response ?: "Error!!!!!"
+    }
+}
+
+fun main() {
+    // Create an instance of RestTemplate
+    val restTemplate = RestTemplate()
+
+    // Create an instance of HttpClient
+    val httpClient = HttpClient(restTemplate)
+
+    // Define API URL and parameters
+    val apiUrl = "https://new.land.naver.com/api/complexes/single-markers/2.0"
+    val parameters = mapOf(
+        "cortarNo" to "4113511100",
+        "zoom" to "16",
+        "priceType" to "RETAIL",
+        "markerId" to "",
+        "markerType" to "",
+        "selectedComplexNo" to "",
+        "selectedComplexBuildingNo" to "",
+        "fakeComplexMarker" to "",
+        "realEstateType" to "APT:ABYG:JGC:PRE",
+        "tradeType" to "A1",
+        "rentPriceMin" to "0",
+        "rentPriceMax" to "900000000",
+        "priceMin" to "100000",
+        "priceMax" to "120000",
+        "areaMin" to "0",
+        "areaMax" to "900000000",
+        "oldBuildYears" to "",
+        "recentlyBuildYears" to "",
+        "minHouseHoldCount" to "",
+        "maxHouseHoldCount" to "",
+        "showArticle" to "false",
+        "sameAddressGroup" to "false",
+        "minMaintenanceCost" to "",
+        "maxMaintenanceCost" to "",
+        "directions" to "",
+        "leftLon" to "127.0881899",
+        "rightLon" to "127.1242388",
+        "topLat" to "37.3612162",
+        "bottomLat" to "37.347588",
+        "isPresale" to "true"
+    )
+
+    // Call the external API
+    val response = httpClient.callExternalGet(apiUrl, parameters)
+
+    // Process the response as needed
+    println("Response from the external API: $response")
+}
