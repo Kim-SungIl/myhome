5d0f6ef900fe679dceb9de571d53ff8b86d118ba
kevin.kim <joongjum@naver.com>
2023-12-02 00:26:10 +0900

    Controller 추가

--- src/main/kotlin/com/example/myhome/controller/MyHouseController.kt
+++ src/main/kotlin/com/example/myhome/controller/MyHouseController.kt
@@ -1,6 +1,5 @@
 package com.example.myhome.controller
 
-import com.example.myhome.component.HttpClient
 import com.example.myhome.service.MyHouseService
 
 class MyHouseController(private val myHouseService: MyHouseService) {
