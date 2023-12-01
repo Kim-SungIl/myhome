c7cae56f0f1542164a2ce235541652c9810297aa
kevin.kim <joongjum@naver.com>
2023-12-02 00:24:27 +0900

    House 클래스 추가

--- src/main/kotlin/com/example/myhome/service/MyHouseService.kt
+++ src/main/kotlin/com/example/myhome/service/MyHouseService.kt
@@ -1,7 +1,12 @@
 package com.example.myhome.service
 
+import com.example.myhome.component.HttpClient
+import com.example.myhome.model.House
 import org.springframework.stereotype.Service
 
 @Service
-class MyHouseService {
+class MyHouseService (private val httpClient:HttpClient){
+    fun findHouses () :List<House>{
+        return ArrayList<House>();
+    }
 }
