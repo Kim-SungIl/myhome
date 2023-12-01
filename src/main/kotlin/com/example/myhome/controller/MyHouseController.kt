fd32530b10e06580744ee38301574f86d7d1bf71
kevin.kim <joongjum@naver.com>
2023-12-02 00:25:40 +0900

    Controller 추가

---
+++ src/main/kotlin/com/example/myhome/controller/MyHouseController.kt
@@ -0,0 +1,7 @@
+package com.example.myhome.controller
+
+import com.example.myhome.component.HttpClient
+import com.example.myhome.service.MyHouseService
+
+class MyHouseController(private val myHouseService: MyHouseService) {
+}
